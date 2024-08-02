6093d7aa89fbfad3f088f09b8764dcb379a55267
AshiqAshokan <ashiqappu112@gmail.com>
2024-08-02 10:53:06 +0530

    Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -33,11 +33,15 @@ const Projects = () => {
             <h1 className='text-2xl text-white font-bold mt-10 mb-4'>Comprehensive College Management Web Application</h1>
             <p className='font-light text-slate-200 tracking-tighter mb-4'>Developed a comprehensive college management web application using the MERN stack, WebRTC, Socket.io, Razorpay, RTK Query, and Tailwind CSS. Key features include online video conferencing, real-time chat, group PTA meetings, tuition fee payments, teacher salary management, online examinations, resource sharing, and attendance tracking. Implemented secure user authentication, responsive design,</p>
             <div className='flex flex-wrap mb-5'>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Html</span>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>css</span>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Bootstrap</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>React</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Tailwind CSS</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Redux</span>
               <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Node js</span>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Mysql</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Mongodb</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Socket io</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Webrtc</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Firebase</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>RazorPay</span>
             </div>
             <div className='flex items-center'>
     <p className='text-white mr-2'>Link:</p>
