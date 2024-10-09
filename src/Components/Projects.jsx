e7e052a59ac935e546e5ad35ed8bcade97547cd0
AshiqAshokan <149768834+AshiqAshokan@users.noreply.github.com>
2024-10-09 10:57:16 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -71,11 +71,13 @@ const Projects = () => {
             <p className='font-light text-slate-200 tracking-tighter mb-4'>
 Ministore is an e-commerce website for clothing sales, built with React, Redux for state management, and RTK Query for data fetching. The backend uses Node.js and MongoDB for handling user data, products, and orders. Firebase manages product uploads, while JWT ensures secure authentication. Razorpay is integrated for smooth, secure payments. This project offers a complete e-commerce solution with real-time interactions, a responsive UI, and robust backend functionality for modern online retail</p>
             <div className='flex flex-wrap'>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Html</span>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>css</span>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Bootstrap</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>React</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Redux</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Tailwind CSS</span>
               <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Node Js</span>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Mysql</span>
+              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>MongoDB</span>
+               <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Firebase</span>
+               <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>JWT</span>
               
             </div>
           </div>
