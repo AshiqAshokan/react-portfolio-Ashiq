b1f36e147f289099f29e72c7fdff17bf8e6d01a2
AshiqAshokan <149768834+AshiqAshokan@users.noreply.github.com>
2024-10-09 10:34:45 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -50,34 +50,7 @@ const Projects = () => {
           </div>
         </motion.div>
       </div>
-      <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-4 md:mx-20'>
-        <motion.div 
-             whileInView={{ opacity:1, x:0 }}
-             initial={{ opacity:0, x:-100 }}
-             transition={{ duration:1 }}
-        className='w-full flex justify-start mt-20 '>
-                <div className="max-w-xs rounded overflow-hidden">
-                    <img src={project2} alt='Ecommercepic' />
-                </div>
-            </motion.div>
-        <motion.div
-           whileInView={{ opacity:1, x:0 }}
-           initial={{ opacity:0, x:100 }}
-           transition={{ duration:1 }}
-         className='w-full flex justify-center items-center'>
-          <div>
-            <h1 className='text-2xl text-white font-bold mt-10 mb-4'>Employee Management System</h1>
-            <p className='font-light text-slate-200 tracking-tighter mb-4'>A fully functional Employee Management System with features like Salary Details, Attendance Details,Company Data and user authentication.</p>
-            <div className='flex flex-wrap'>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Html</span>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>css</span>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Bootstrap</span>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Node js</span>
-              <span className='mr-2 mt-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800'>Mongodb</span>
-            </div>
-          </div>
-        </motion.div>
-      </div>
+     
       <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-4 md:mx-20'>
         <motion.div 
              whileInView={{ opacity:1, x:0 }}
